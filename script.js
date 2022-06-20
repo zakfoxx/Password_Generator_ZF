650b10084fc8f5c47b6e76c85fdbc7d7209ae470
Zechariah Foxx <zak.foxx@gmail.com>
2022-06-20 18:33:07 -0400

    created prompts

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+var promptChar = window.prompt(
+  "How many characters would you like in your password? (Must be between 8 to 128 characters.)"
+);
 
+var promptLower = window.prompt(
+  "Would you like your password to contain lowercase letters?"
+);
+
+var promptUpper = window.prompt(
+  "Would you like your password to contain uppercase letters?"
+);
+
+var promptNumbers = window.prompt(
+  "Would you like your password to contain numbers?"
+);
+
+var promptSpecial = window.prompt(
+  "Would you like your password to contain special characters?"
+);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +28,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
